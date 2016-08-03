545179e80bf05a5344fed59664c49fe75cb94353
Ivan Baena <ivan.baena04@gmail.com>
2016-08-03 13:07:54 -0400

    form properly submitting and sending email on speciified email template

--- server.js
+++ server.js
@@ -2,10 +2,25 @@
 
 let express = require('express');
 let app = express();
+let mailer = require('express-mailer');
 let exphbs = require('express-handlebars');
 let path = require('path');
 let fs = require('fs');
 let bodyParser = require('body-parser');
+
+//Email Setup
+mailer.extend(app, {
+  from: 'no-reply@example.com',
+  host: 'smtp.gmail.com', // hostname
+  secureConnection: true, // use SSL
+  port: 465, // port for secure SMTP
+  transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
+  auth: {
+    user: '',              //User Creds
+    pass: ''               //User Creds
+  }
+});
+
 
 //Templating engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -26,10 +41,35 @@ app.listen(PORT, function() {
   console.log("Listening on PORT %s", PORT);
 });
 
+
+
 //Routes
  app.get('/', function(req, res){
    res.render('form', {
         title : 'Form Submission!',
         layout : 'main'
      });
+ });
+ app.post('/', function (req, res, next) {
+   console.log(req.body);
+
+   let email = req.body.email;
+   let name = req.body.name;
+   let phone = req.body.phone;
+
+   app.mailer.send('email', {
+     to: email, // REQUIRED. This can be a comma delimited string just like a normal email to field.
+     subject: 'New Lead!', // REQUIRED.
+     name: name,
+     phone: phone,
+     email: email
+   }, function (err) {
+     if (err) {
+       // handle error
+       console.log(err);
+       res.send('There was an error sending the email');
+       return;
+     }
+     res.send('Email Sent');
+   });
  });
