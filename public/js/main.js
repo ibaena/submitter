6f1f0e46ae33acbab76b7508eb75204adaf7950e
Matt Urenovich <matt.urenovich@gmail.com>
2016-09-28 15:29:20 -0400

    Finalized Demo

--- public/js/main.js
+++ public/js/main.js
@@ -26,15 +26,48 @@ $(document).ready(function() {
         // Fires off snippet for second image
         setTimeout(function(){
             if (unwrapBtm && unwrapTop === true){
-                $('#bottom-media').addClass('animated slideOutLeft soft-hide');
+                $('#bottom-media').addClass('animated slideOutLeft');
+                
 
                 // Fires bottom wrapper on second image
                 setTimeout(function() {
-                    $('#top-media').addClass('animated slideOutRight soft-hide');
+                    $('#top-media').addClass('animated slideOutRight');
+                   
 
                 }, 700)
             }
         }, 2000)
+
+        // Fires off snippet for third image
+        setTimeout(function(){
+                $('#stetson-btm-wrapper').addClass('animated slideOutLeft');
+                // Fires bottom wrapper on second image
+                setTimeout(function() {
+                    $('#stetson-top-wrapper').addClass('animated slideOutRight');
+
+                }, 700)
+        }, 4000)
+
+        // Fires off snippet for fourth image
+        setTimeout(function(){
+                $('#addidas-btm-wrapper').addClass('animated slideOutLeft');
+                // Fires bottom wrapper on second image
+                setTimeout(function() {
+                    $('#addidas-top-wrapper').addClass('animated slideOutRight');
+
+                }, 700)
+        }, 6000)
+
+        // Fires off snippet for fifth image
+        setTimeout(function(){
+                $('#jovan-btm-wrapper').addClass('animated slideOutLeft');
+                // Fires bottom wrapper on second image
+                setTimeout(function() {
+                    $('#jovan-top-wrapper').addClass('animated slideOutRight');
+
+                }, 700)
+        }, 8000)
+
 
 
     });
