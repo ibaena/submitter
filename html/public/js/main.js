234a926ad6b77ea693eaa5985d1d582367a4d091
Matt Urenovich <matt.urenovich@gmail.com>
2016-10-03 13:42:23 -0400

    Updated timing and fadeout

--- html/public/js/main.js
+++ html/public/js/main.js
@@ -28,13 +28,13 @@ $(document).ready(function() {
             unwrapTop = true;
         }, 700);
 
-        // Fires off snippet for second image
+        // Fires off snippet for second image (Nautica)
         setTimeout(function() {
             $('#btm-roller').hide();
             if (unwrapBtm && unwrapTop === true) {
-                $('#bottom-media').animate({width:'0%'},500);
-
-                    $('.unwrap-img-left').attr('src', '../public/assets/4way/banner-bg-bottom.png');
+                $('#bottom-media').animate({width:'0%'},750);
+                $('#nautica-img-btm').fadeOut('700').attr('src', '../public/assets/4way/banner-bg-bottom.png');
+                // $('.unwrap-img-left').attr('src', '../public/assets/4way/banner-bg-bottom.png');
 
 
                 $('#btm-roller').show().addClass('animated slideOutLeft');
@@ -45,7 +45,8 @@ $(document).ready(function() {
                     $('#btm-roller').hide();
 
                     $('#top-media').addClass('animated slideOutRight');
-                    $('.unwrap-img-right').attr('src', '../public/assets/4way/banner-bg-top.png');
+                    // $('.unwrap-img-right').attr('src', '../public/assets/4way/banner-bg-top.png');
+                    $('#nautica-img-top').fadeOut('700').attr('src', '../public/assets/4way/banner-bg-top.png');
                     $('#top-roller').show().addClass('animated slideOutRight');
                     $('#top-roller-img').addClass('slideRight');
 
@@ -55,18 +56,21 @@ $(document).ready(function() {
             }
         }, 2000)
 
-        // Fires off snippet for third image
+        // Fires off snippet for third image (Stetson)
         setTimeout(function() {
-            $('#stetson-btm-wrapper').animate({width:'0%'},500);
-            $('#stetson-img-btm').fadeOut('2000');
+            $('#stetson-btm-wrapper').animate({width:'0%'},750);
+            $('.stetson-unwrap-img-left').attr('src', '../public/assets/4way/banner-bg-bottom.png');
+            $('#stetson-img-btm').fadeOut('700');
             $('#btm-roller').show().addClass('animated slideOutLeft');
             $('#btm-roller-img').addClass('slideLeft');
+            
             // Fires bottom wrapper on second image
             setTimeout(function() {
                 $('#btm-roller').hide();
 
                 $('#stetson-top-wrapper').addClass('animated slideOutRight');
-                $('#stetson-img-top').fadeOut('2000');
+                $('.stetson-unwrap-img-right').attr('src', '../public/assets/4way/banner-bg-top.png');
+                $('#stetson-img-top').fadeOut('700');
                 $('#top-roller').show().addClass('animated slideOutRight');
                 $('#top-roller-img').addClass('slideRight');
 
@@ -74,10 +78,10 @@ $(document).ready(function() {
             $('#top-roller').hide();
         }, 4000)
 
-        // Fires off snippet for fourth image
+        // Fires off snippet for fourth image (Adidas)
         setTimeout(function() {
             $('#addidas-btm-wrapper').animate({width:'0%'},500);
-            $('#addidas-img-btm').fadeOut('2000');
+            $('#addidas-img-btm').fadeOut('1000');
             $('#btm-roller').show().addClass('animated slideOutLeft');
             $('#btm-roller-img').addClass('slideLeft');
             // Fires bottom wrapper on second image
@@ -85,7 +89,7 @@ $(document).ready(function() {
                 $('#btm-roller').hide();
 
                 $('#addidas-top-wrapper').addClass('animated slideOutRight');
-                $('#addidas-img-top').fadeOut('2000');
+                $('#addidas-img-top').fadeOut('1000');
                 $('#top-roller').show().addClass('animated slideOutRight');
                 $('#top-roller-img').addClass('slideRight');
 
@@ -93,10 +97,10 @@ $(document).ready(function() {
             $('#top-roller').hide();
         }, 6000)
 
-        // Fires off snippet for fifth image
+        // Fires off snippet for fifth image (Jovan)
         setTimeout(function() {
             $('#jovan-btm-wrapper').animate({width:'0%'},500);
-            $('#jovan-img-btm').fadeOut('2000');
+            $('#jovan-img-btm').fadeOut('1000');
             $('#btm-roller').show().addClass('animated slideOutLeft');
             $('#btm-roller-img').addClass('slideLeft');
             // Fires bottom wrapper on second image
@@ -104,7 +108,7 @@ $(document).ready(function() {
                 $('#btm-roller').hide();
 
                 $('#jovan-top-wrapper').addClass('animated slideOutRight');
-                $('#jovan-img-top').fadeOut('2000');
+                $('#jovan-img-top').fadeOut('1000');
                 $('#top-roller').show().addClass('animated slideOutRight');
                 $('#top-roller-img').addClass('slideRight');
 
@@ -112,7 +116,7 @@ $(document).ready(function() {
             $('#top-roller').hide();
         }, 8000)
 
-        // Fires off snippet for fifth image
+        // Fires off snippet for fifth image (3 Gift Sets)
         setTimeout(function() {
             $('#final-wrapper').addClass('animateds slideOutLeft');
             // Fires bottom wrapper on second image
